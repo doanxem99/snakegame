6fc728c9cf7f5c7178a0ef7ce535abc838ca99dd
doanxem99 <96615248+doanxem99@users.noreply.github.com>
2023-08-02 21:04:35 +0700

    Add files via upload
    updateWebsite

--- website.js
+++ website.js
@@ -10,6 +10,9 @@ function clickAbout(){
     document.querySelector('.about').classList.add('aboutactive');
     onAbout++;
 }
+function broadenSearch(){
+    document.querySelector('.search').style = "width: 60%; visibility: visible";
+}
 var imgFeature = document.querySelector('.imgFeature');
 var imgAsL = document.querySelector('.L');
 var imgAsR = document.querySelector('.R');
@@ -27,20 +30,53 @@ function Calc(index, operate)
     else
         return (index - 1 < 0 ? listImg.length - 1 : index - 1);
 }
-function updateImageByIndex(index){
-    nextSlide = 0;
+function highlightImage(index){
     document.querySelectorAll('.listImage div').forEach(imgItem => {
         imgItem.classList.remove('active');
     });
+    listImg[index].parentElement.classList.add('active');
+}
+function updateImageByIndex(index){
+    nextSlide = 0;
     imgAsL.src = listImg[Calc(index, -1)].getAttribute('src');
     imgAsR.src = listImg[Calc(index, 1)].getAttribute('src');
-    listImg[index].parentElement.classList.add('active');
     imgFeature.style.opacity = '0';
+    highlightImage(index);
     setTimeout(()=>{
         imgFeature.src = listImg[index].getAttribute('src');
         imgFeature.style.opacity = '1';
     }, 400);
 }
+var tmp = imgAsLL;
+tmp.src = imgAsLL.src;
+function returnNormal_moveRight(){
+    imgAsRR.src = imgAsR.src; 
+    imgAsR.src = imgFeature.src;
+    imgFeature.src = imgAsL.src;
+    imgAsL.src = imgAsLL.src;
+    
+    imgAsRR.style = imgAsR.style;
+    imgAsR.style = imgFeature.style;
+    imgFeature.style = imgAsL.style;
+    imgAsL.style = imgAsLL.style;
+    
+    imgAsLL.style = tmp.style;
+    imgAsLL.src = tmp.src;
+}
+function updateImageByAutos(index){
+    imgFeature.style.animation = 'moveRightAs 2s ease-in-out forwards';
+    setTimeout(() => {
+        imgAsL.style.animation = 'moveRightMain 2s ease-in-out forwards';
+        imgAsLL.style.animation = 'moveRight 1s ease-in-out forwards';
+        imgAsR.style.animation = 'moveRight 1s ease-in-out forwards';
+        imgAsRR.style.animation = 'moveRight 1s ease-in-out forwards';}
+        , 1000);
+        setTimeout(() => {
+            returnNormal_moveRight();
+            tmp = imgAsLL;
+            tmp.src = listImg[index].src;
+        }, 3000);
+    }
 listImg.forEach((imgElement, index) => {
     imgElement.addEventListener('click', e =>{
         updateImageByIndex(index);
@@ -59,11 +95,14 @@ prevBtn.addEventListener('click', e => {
 moveSlide();
 function moveSlide(){
     if (nextSlide == 1){
-        updateImageByIndex(current = (++current == listImg.length) ? 0 : current);
-        moveSlide();
+        updateImageByAutos((current - 3 < 0) ? listImg.length - (3 - current) : current - 3);
+        current--;
+        if (current < 0) current = listImg.length - 1;
+        highlightImage(current);
+        setTimeout(() => {moveSlide()}, 8000);
     } 
     else {
-        setTimeout(() => {moveSlide()}, nextSlide == -1 ? 100000 : 3000);
+        setTimeout(() => {moveSlide()}, nextSlide == -1 ? 15000 : 8000);
         nextSlide = 1;
     }
 }
